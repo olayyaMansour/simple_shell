537edd2f35d570322c0b7d12ab0b42ddcac65c20
olayyaMansour <mansour.olayya@gmail.com>
2023-11-29 18:54:05 -0800

    Add explanatory comments to split_line.c

--- split_line.c
+++ split_line.c
@@ -7,7 +7,9 @@
  */
 char **splitLine(char *line)
 {
-	char **args = malloc(2 * sizeof(char *));
+	/* For now the command lines are made only of one word. No arguments will be passed to programs. So we are just splitting the line into two arguments (command and NULL) and returning it. */
+
+	char **args = malloc(2 * sizeof(char *)); // 2 is the number of arguments (command and NULL)
 
 	args[0] = line;
 	args[1] = NULL;
