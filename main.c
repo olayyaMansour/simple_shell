46c9f8c64b9de61e9c15f44f7595587ab93c89bd
olayyaMansour <mansour.olayya@gmail.com>
2023-11-29 18:48:17 -0800

    Add explanatory comments to main.c

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(void)
 		displayPrompt();
 		line = readLine();
 		args = splitLine(line);
-		status = execute(args);
+		status = execute(args); //returns 0 on success and a non-zero value on failure, so we can use it as the condition for the do-while loop to keep the shell running until the user types exit or presses Ctrl+D to exit the shell.
 		free(line);
 		free(args);
 	} while (status);
