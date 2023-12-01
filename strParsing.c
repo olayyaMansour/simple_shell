3ef55c73d3074d2a8ad5739e471a0af7bd88048c
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-12-01 13:57:48 -0800

    Added function that tokenized a string

---
+++ strParsing.c
@@ -0,0 +1,47 @@
+#include "parse.h"
+/**
+ * strParsing - tokenizes a string into an arr of strings
+ * @data: string to be tokenized
+ * @dataContainer: duplication of string
+ * @token: temp var to hold token
+ * @Cmd: array to store the tokens
+ *
+ * Return: array of strings or NULL
+ * if input is NULL or fails to allocate memory
+ */
+char **strParsing(char *data, char *dataContainer, char *token, char **Cmd)
+{
+	short i = 0, limiter = 1;
+
+	if (!data)
+		return (NULL);
+
+	dataContainer = _strDuplicate(data);
+	token = strtok(dataContainer, DELIMITER);
+
+	Cmd = _INIT_TOKENS(&token, &limiter);
+
+	if (!Cmd)
+	{
+		while (token != NULL)
+		{
+		Cmd = parseToken(Cmd, &i, &limiter, token);
+		if (!Cmd)
+		{
+		_freeMemo(dataContainer, data);
+		return (NULL);
+		}
+		token = strtok(NULL, DELIMITER);
+		}
+	}
+	else
+	{
+	_freeMemo(dataContainer, data);
+	return (NULL);
+	}
+
+	_freeMemo(dataContainer, data);
+	Cmd[i] = '\0';
+
+	return (Cmd);
+}
