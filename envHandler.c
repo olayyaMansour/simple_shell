ffddd57e4094f397f098c6fffce304533c28b152
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-12-05 11:18:00 -0800

    Added function that retrieves value of an env var

---
+++ envHandler.c
@@ -0,0 +1,35 @@
+#include "MemoHandlers.h"
+#include "dataHandlers.h"
+#include "strLib.h"
+/**
+ * envHandler - retrieves value of an environment var
+ * @envName: name of environment variable
+ * @envValue: value of environment variable
+ * @envKey: key of environment variable
+ * Return:
+ * value of env variable
+ * Or
+ * NULL if not found
+ */
+char **envHandler(char *envName, char *envValue, char *envKey)
+{
+	char *Temp = NULL;
+
+	while (*environ != NULL)
+	{
+		temp = _strDuplicate(*environ);
+		envKey = strtok(Temp, "=");
+
+		if (!(_strCompare(envKey, envName) && envKey))
+		{
+		envValue = _strDuplicate(strtok(NULL, "\n"));
+		_freeMemo(Temp, NULL);
+		return ((char **)envValue);
+		}
+
+	_freeMemo(Temp, NULL);
+	environ++;
+	}
+
+	return (NULL);
+}
