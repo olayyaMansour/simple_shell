9e682b4ea521534195086ba64047de29af7bcfe2
olayyaMansour <mansour.olayya@gmail.com>
2023-12-10 05:51:42 -0800

    Update splitLine without strtok

--- split_line.c
+++ split_line.c
@@ -1,16 +1,19 @@
 #include "main.h"
+
 /**
  * splitLine - split a line into arguments
  * @line: input line
  *
  * Return: array of arguments
  */
-char **splitLine(char *line)
+char** splitLine(char* line)
 {
 	size_t bufsize = BUFFER_SIZE;
 	size_t position = 0;
-	char **args = malloc(bufsize * sizeof(char *));
-	char *token;
+	char** args = malloc(bufsize * sizeof(char*));
+	char* token = NULL;
+	char* delimiter = " \t\n\r\a";
+	char *arg_start;
 
 	if (!args)
 	{
@@ -18,26 +21,45 @@ char **splitLine(char *line)
 		exit(EXIT_FAILURE);
 	}
 
-	token = strtok(line, " \t\n\r\a");
-	while (token != NULL)
+	token = line;
+	while (*token != '\0')
 	{
-		args[position] = token;
+		while (*token != '\0' && strchr(delimiter, *token) != NULL)
+			token++;
+
+		if (*token == '\0')
+			break;
+
+		arg_start = token;
+
+		while (*token != '\0' && strchr(delimiter, *token) == NULL)
+			token++;
+
+		args[position] = malloc((token - arg_start + 1) * sizeof(char));
+
+		if (args[position] == NULL)
+		{
+			perror("malloc error");
+			exit(EXIT_FAILURE);
+		}
+
+		strncpy(args[position], arg_start, token - arg_start);
+		args[position][token - arg_start] = '\0';
 		position++;
 
 		if (position >= bufsize)
 		{
 			bufsize += BUFFER_SIZE;
-			args = realloc(args, bufsize * sizeof(char *));
+			args = realloc(args, bufsize * sizeof(char*));
 			if (!args)
 			{
 				perror("realloc error");
 				exit(EXIT_FAILURE);
 			}
 		}
-
-		token = strtok(NULL, " \t\n\r\a");
 	}
 
 	args[position] = NULL;
-	return (args);
+	return args;
 }
+
