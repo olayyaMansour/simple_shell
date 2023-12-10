d8b7b62008861a83a080f66b2ae95cd23a5be021
GodSlayerMode <ssadgali2@gmail.com>
2023-12-10 21:42:15 +0100

    Fixed the non-interactive mode

--- data_reader.c
+++ data_reader.c
@@ -10,24 +10,33 @@ char *ReadData(const char *Message)
 	char *Cmd = NULL;
 	size_t sizeOfBuffer = 0;
 	ssize_t Length;
+	int log = 0;
 
+	if (isatty(STDIN_FILENO))
+	{
 	write(STDOUT_FILENO, Message, strlen(Message));
+	}
 
 	Length = getline(&Cmd, &sizeOfBuffer, stdin);
 
 	if (Length > 0 && Cmd[Length - 1] == '\n')
 	{
-		Cmd[Length - 1] = '\0';
+	Cmd[Length - 1] = '\0';
 	}
 	else if (Length == -1)
 	{
-		if (isatty(STDIN_FILENO))
-		{
-		write(STDOUT_FILENO, "\n", sizeof("\n"));
-		free(Cmd);
-		onExit();
-		}
+	if (isatty(STDIN_FILENO))
+	{
+	write(STDOUT_FILENO, "\n", 1);
+	_freeMemo(Cmd, NULL);
+	onExit(NULL, log);
+	}
+	else
+	{
+	_freeMemo(Cmd, NULL);
+	onExit(NULL, log);
+	}
 	}
 
-	return (Cmd);
+	return Cmd;
 }
